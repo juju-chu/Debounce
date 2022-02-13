7e13bf3f56eca35e58a5ee8b4d5adf6c4e38d8f2
Juju <jujuchu@pixnet.tw>
2022-02-13 17:58:47 +0800

    feat: Debounce 應用在文字輸入，2秒後在 console 印出輸入結果

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const delayTime = 300;
+const delayTime = 2000
 
+// func: 要執行的 function
+// delay: 延遲幾秒才執行 
 const debounceFunc = function debounce(func, delay) {
-    let timeout;
+    let timeout
     return function () {
-        const later = function () {
-            func()
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, delay)
-    };
-};
+        const context = this  // 指向 debounceInput
+        const args = arguments  // KeyboardEvent
 
-function doSomething() {
-    console.log('doSomething');
+        // 延遲後要執行的動作
+        const later = function () {
+            func.apply(context, args)
+        }
+
+        // 重新計時
+        clearTimeout(timeout)
+        timeout = setTimeout(later, delay)
+    }
 }
 
-debounceFunc(doSomething, delayTime)();
+const debounceInput = document.getElementById('debounce-input');
+
+// 輸入字
+debounceInput.addEventListener('keyup', debounceFunc((e) => {
+    console.log(e.target.value);
+}, delayTime))
